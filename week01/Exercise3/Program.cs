248113640ca4c195e4d9132f1259cc58d017db87
Kermicha Philippe <kermicha007@gmail.com>
2026-07-04 17:31:51 -0400

    work

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.Write("What is the magic number? ");
+        string input = Console.ReadLine();
+        int magicNumber = int.Parse(input);
+
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+        int guess = int.Parse(guessInput);
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
