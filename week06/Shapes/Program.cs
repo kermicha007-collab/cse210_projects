cd9147eb1967e5f0b1258fcc10b2f2c00cc2536b
Kermicha Philippe <kermicha007@gmail.com>
2026-08-09 20:11:56 -0400

    done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("Blue", 4);
+        Console.WriteLine($"The area of the square is: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Red", 5, 3);
+        Console.WriteLine($"The area of the rectangle is: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 2.5);
+        Console.WriteLine($"The area of the circle is: {circle.GetArea()}");
     }
 }
